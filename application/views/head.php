d79399f86c673549366b58627056be18c71a5bd1
sanjay raj <sanjay.raj@siqsess.com>
2023-06-17 21:18:56 +0530

    theme integration

---
+++ application/views/head.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="utf-8">
+    <title>Seomx - Seo & Digital Marketing HTML Template</title>
+
+    <meta name="author" content="themesflat.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/dist/font-awesome.css'); ?>">
+    
+    <link rel="stylesheet" href="<?php echo base_url('assets/dist/app.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/dist/responsive.css'); ?>">
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <!-- Favicon and Touch Icons  -->
+    <link rel="shortcut icon" href="assets/images/logo/Favicon.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/images/logo/Favicon.png">
+
+</head>
